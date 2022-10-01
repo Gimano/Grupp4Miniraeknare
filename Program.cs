5fecda98d89fa4379385413e940f6e4aa3e4457d
Gimano <112472723+Gimano@users.noreply.github.com>
2022-10-02 01:15:20 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using Grupp4Miniräknare;
+
+bool running = true;
+while (running)
+{
+    Console.WriteLine("Välkommen till våra miniräknare!");
+    Console.WriteLine("Välj ett alternativ:");
+    Console.WriteLine("1. Martins miniräknare");
+    Console.WriteLine("2. Markus miniräknare");
+    Console.WriteLine("3. Krittapats miniräknare");
+    Console.WriteLine("4. Exit");
+    Console.WriteLine("--------------------------------");
+    int val = 0;
+    if (int.TryParse(Console.ReadLine(), out int result))
+        val = result;
+
+    switch (val)
+    {
+        case 1:
+            MartinKalkylator.Program();
+            break;
+        case 2:
+            break;
+        case 3:
+            break;
+        case 4:
+            running = false;
+            break;
+        default:
+            Console.Clear();
+            Console.WriteLine("\nEj ett valbart alternativ.\n");
+            break;
+    }
+}
